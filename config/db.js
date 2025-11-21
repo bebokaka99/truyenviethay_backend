72dd241ac330052e37e9ef88ad08a96808dd9a42
bebokaka99 <nq20k2@gmail.com>
2025-11-21 20:09:20 +0700

    test

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
+// Không cần import fs vì ta đọc từ ENV
+// const fs = require('fs'); 
 
 dotenv.config();
 
@@ -11,13 +13,21 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    timezone: '+07:00' // <--- THÊM DÒNG NÀY QUAN TRỌNG NHẤT
+    timezone: '+07:00', 
+    
+    // --- BẮT BUỘC PHẢI THÊM ĐOẠN NÀY CHO TI-DB/PLANETSCALE ---
+    ssl: {
+        // Node.js sẽ coi chuỗi này là nội dung của Certificate
+        ca: process.env.TIDB_CA_CERT 
+    }
 });
 
 // Kiểm tra kết nối
 pool.getConnection((err, connection) => {
     if (err) {
         console.error('❌ Lỗi kết nối Database:', err.message);
+        console.error('=> Lỗi 1: Kiểm tra lại biến môi trường.');
+        console.error('=> Lỗi 2: Kiểm tra nội dung TIDB_CA_CERT có đầy đủ không.');
     } else {
         console.log('✅ Đã kết nối Database MySQL (Timezone +07:00)!');
         connection.release();
